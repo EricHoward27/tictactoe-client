c71f9199140f718bc78b7818694fd4ecd2de6827
Eric <howarderic585@gmail.com>
2021-02-12 11:24:24 -0600

    Fix apiUrl set to development and save api response into a variable

--- assets/scripts/game/api.js
+++ assets/scripts/game/api.js
@@ -3,16 +3,19 @@ const config = require('../config')
 // empty object to store user data
 const store = require('./../store')
 // start a new game
-const newGame = (playerStart) => {
-  store.playerOne = 'X'
-  store.playerCounter = 0
+const newGame = (data) => {
+  // start player at X
+  store.player = 'X'
+  // set the counter at 0
+  store.counter = 0
   return $.ajax({
-    url: config.apiUrl + '/games',
+    url: config.apiUrl.development + '/games',
     method: 'POST',
     headers: {
-      Authorization: 'Token token= ' + store.user.token
+      Authorization: 'Bearer= ' + store.user.token
     }
   })
+  // saving the api response to have access to game object
     .then((response) => {
       store.game = response.game
     })
